fd5d8783f78f9e15348f6d7d6b0ea288a8017674
sonpipe0 <pipefc2003@gmail.com>
2025-03-11 18:38:56 -0300

    added listener and println

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3030")?;
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
+}
+
+fn handle_client(stream: TcpStream) {
+    println!("hello");
 }
