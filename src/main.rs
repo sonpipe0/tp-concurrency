fcb80d5317aa80201195be67c26850d30ec29308
sonpipe0 <pipefc2003@gmail.com>
2025-03-12 16:13:55 -0300

    detect end of headers

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use std::net::{TcpListener, TcpStream};
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:3030")?;
     for stream in listener.incoming() {
-        handle_client(stream?);
+        handle_client(stream?).unwrap();
     }
     Ok(())
 }
 
-fn handle_client(stream: TcpStream) {
-    println!("hello");
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buf = [0; 512];
+    let bytes = stream.read(&mut buf).unwrap();
+    let req = String::from_utf8_lossy(&buf[..bytes]);
+    println!("{}", req); // los headers terminan con un /r/r/n/n
+    let (headers, body) = req.split_at(req.rfind("\r\n\r\n").expect("Headers not found"));
+    println!("headers:{},body:{}", headers, body);
+    Ok(())
 }
