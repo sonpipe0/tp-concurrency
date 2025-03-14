60a6de084cea4232e2a5bed8ef9bb8b797f34734
ManuLosta <mlostalom@gmail.com>
2025-03-14 17:39:42 -0300

    refactor how connection is handled

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
 use std::{
-    io::Read,
+    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:3030")?;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:3030").unwrap();
     for stream in listener.incoming() {
-        handle_client(stream?).unwrap();
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
     }
-    Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
-    let mut buf = [0; 512];
-    let bytes = stream.read(&mut buf).unwrap();
-    let req = String::from_utf8_lossy(&buf[..bytes]);
-    println!("{}", req); // los headers terminan con un /r/r/n/n
-    let (headers, body) = req.split_at(req.rfind("\r\n\r\n").expect("Headers not found"));
-    println!("headers:{},body:{}", headers, body);
-    Ok(())
+fn handle_connection(stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
 }
